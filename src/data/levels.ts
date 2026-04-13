44bb63ca83db0b179323b067927e518a42d26ca5
Arnav Jha <overrideeducation@gmail.com>
2026-04-13 16:19:01 -0700

    update aarush and ruhaan's links

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -17,7 +17,7 @@ const levels: LevelConfig[] = [
     nextClass: {
       startTime: "2026-04-13T19:00:00-05:00",
       durationMinutes: 60,
-      classLink: "https://meet.google.com/diz-hqgy-nwb",
+      classLink: "https://meet.google.com/prv-qefy-jfb",
     },
   },
   {
@@ -59,7 +59,7 @@ const levels: LevelConfig[] = [
     nextClass: {
       startTime: "2026-04-13T19:00:00-05:00",
       durationMinutes: 60,
-      classLink: "https://meet.google.com/prv-qefy-jfb",
+      classLink: "https://meet.google.com/obk-ekfg-vmq",
     },
   },
   {
