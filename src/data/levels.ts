c58a5a422cb51a7257368caef6596ae860f9f28f
Arnav Jha <overrideeducation@gmail.com>
2026-06-22 16:46:45 -0700

    update 6/22 classes

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -15,7 +15,7 @@ const levels: LevelConfig[] = [
     githubLink: "about:blank",
     whatsappLink: "https://chat.whatsapp.com/DzYcETjfoas1Xx757jraJD",
     nextClass: {
-      startTime: "2026-06-19T09:00:00-15:00",
+      startTime: "2026-06-22T09:00:00-05:00",
       durationMinutes: 60,
       classLink: "https://meet.google.com/dro-spgf-jje",
     },
@@ -36,7 +36,7 @@ const levels: LevelConfig[] = [
     githubLink: "about:blank",
     whatsappLink: "https://chat.whatsapp.com/DzYcETjfoas1Xx757jraJD",
     nextClass: {
-      startTime: "2026-06-19T09:00:00-15:00",
+      startTime: "2026-06-22T09:00:00-05:00",
       durationMinutes: 60,
       classLink: "https://meet.google.com/mks-zyxa-usi",
     },
@@ -57,9 +57,9 @@ const levels: LevelConfig[] = [
     githubLink: "about:blank",
     whatsappLink: "https://chat.whatsapp.com/DzYcETjfoas1Xx757jraJD",
     nextClass: {
-      startTime: "2026-06-19T10:00:00-15:00",
+      startTime: "2026-06-22T10:00:00-05:00",
       durationMinutes: 60,
-      classLink: "https://meet.google.com/wps-wvym-vrm",
+      classLink: "https://meet.google.com/fwu-fsro-izy",
     },
   },
 ];
